11ef0359138a5f1f63310e4dce08090fbceb153b
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2023-06-02 20:52:11 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,11 +1,15 @@
 Config = {}
 
-Config.MaxDistance = 5 -- Distance from the hanging zone
+Config.MaxDistance = 8 -- Distance from the hanging zone
 Config.TooFar = "You Are Too Far To Do This Command !"
 Config.Text = "You have been Hanged and will ~e~Die~q~ Soon !"
 
--- where you want the hand spot for prisonner
+-- where you want the hang spot for prisonner
 Config.Hang = {
- { x = -314.21, y = 728.95, z = 120.65, h = 97.07 }
-    
+ { x = -314.21, y = 728.95, z = 120.65, h = 97.07 }, -- Valentine
+ { x = -1806.69, y = -333.83, z = 167.65, h = 265.58 }, -- Strawberry
+ { x = -764.78, y = -1259.9, z = 47.39, h = 4.98 }, -- Blackwater
+ { x = 1374.65, y = -1215.41, z = 84.23, h = 84.88 }, -- Rhodes
+ { x = 2687.65, y = -1113.73, z = 52.87, h = 177.44 }, -- Saint Denis
+ { x = 2905.99, y = 1300.01, z = 47.87, h = 247.21 } -- Annesburg
 }
